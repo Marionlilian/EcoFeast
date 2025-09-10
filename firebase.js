7f0b5f885cc2e8208d697ea5cc1fedd77c8e41de
marionlilian <marionwangui49@gmail.com>
2025-09-10 12:30:15 +0300

    firebase config

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6w9UXbSwSZ7Aw8ZUhrxt-bHnMu3k2xYE",
-  authDomain: "ecofeast-5ff01.firebaseapp.com",
-  projectId: "ecofeast-5ff01",
-  storageBucket: "ecofeast-5ff01.firebasestorage.app",
-  messagingSenderId: "111524926453",
-  appId: "1:111524926453:web:cad7d24952f5f4be5a06bc",
-  measurementId: "G-RCM88L20YL"
+  apiKey: "import.meta.env.VITE_OPENAI_API_KEY",
+  authDomain: "import.meta.env.VITE_FIREBASE_AUTH_DOMAIN",
+  projectId: "import.meta.env.VITE_FIREBASE_PROJECT_ID",
+  storageBucket: "import.meta.env.VITE_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "import.meta.env. VITE_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "import.meta.env. VITE_FIREBASE_APP_ID",
+  measurementId: "import.meta.env.VITE_FIREBASE_MEASUREMENT_ID"
 };
 
 // Initialize Firebase
