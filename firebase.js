bcba90a8d6006c9ec14413c82135cf9b6c43df19
marionlilian <marionwangui49@gmail.com>
2025-09-11 10:27:40 +0300

    set-up

--- firebase.js
+++ firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp} from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { Firestore, getFirestore } from "firebase/firestore";
+
+
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,16 +11,16 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "import.meta.env.VITE_OPENAI_API_KEY",
-  authDomain: "import.meta.env.VITE_FIREBASE_AUTH_DOMAIN",
-  projectId: "import.meta.env.VITE_FIREBASE_PROJECT_ID",
-  storageBucket: "import.meta.env.VITE_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "import.meta.env. VITE_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "import.meta.env. VITE_FIREBASE_APP_ID",
-  measurementId: "import.meta.env.VITE_FIREBASE_MEASUREMENT_ID"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db= getFirestore(app)
 export const auth =getAuth(app);
 // const analytics = getAnalytics(app);
--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,34 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import { Route,Routes } from 'react-router-dom'
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword} from 'firebase/auth'
 import { getAuth, signOut } from 'firebase/auth'
 import { auth } from '../firebase';
+import Login from './assets/Login'
+import AdminDashboard from './admin/AdminDashboard'
+import NgoDashboard from './Ngo/NgoDashboard'
+import UserDashboard from './userdash/UserDashboard'
+import Home from './Home'
 
 function App() {
-const [registerEmail, setRegisterEmail]=useState('');
-const [registerPassword, setRegisterPassword]=useState('')
 
-const [loginEmail, setLoginEmail]=useState('');
-const [loginPassword, setLoginPassword]=useState('')
-
-const [user, setUser]= useState({})
-
-
-onAuthStateChanged (auth, (currentUser)=>{
-  setUser(currentUser)
-})
-
-
-
-  const register = async()=>{
-    try {
-      const user = await createUserWithEmailAndPassword (auth, registerEmail, registerPassword);
-      console.log (user);
-     
-    } catch (error) {
-      console.log (error.message)
-      
-      
-    } 
-
-  }
-  const login = async()=>{
-    try {
-      const user = await signInWithEmailAndPassword (auth, loginEmail, loginPassword )
-      
-    } catch (error) {
-      console.log(error.message)
-      
-    }
-
-  }
-const logout= ()=>{
-  signOut(auth)
-
-}
   return (
     <>
-     <BrowserRouter>
-  <Routes>
-    <Route  path='' />
+     
+     <Routes>
+    <Route  path='/' element={<Home/>}/>
+    <Route path= './assets/Login' element={<Login/>}/>
+    <Route path= './admin/AdminDashboard' element={<AdminDashboard/>}/>
+    <Route path='./userdash/UserDashboard' element={<UserDashboard/>}/>
+    <Route path= './Ngo/NgoDashboard' element={<NgoDashboard/>}/>
   </Routes>
-  
-  
-  </BrowserRouter>
+  <Home/>
+  <Login/>
+  <AdminDashboard/>
+  <NgoDashboard/>
+  <UserDashboard/>
+ 
 
-  <div>
-    <h1>NGO Register</h1>
-    <input type="text" onChange={(event) =>{
-      setRegisterEmail(event.target.value)
-    }} />Username 
-    <input type="password" onChange={(event) =>{
-      setRegisterPassword(event.target.value)
-    }} />Password 
-    <button onClick={register}>Register</button>
-  </div>
-  <div>
-    <h1>
-      NGO Login
-    </h1>
-    <input type="text" onChange={(event) =>{
-      setLoginEmail(event.target.value)
-    }} />Username 
-    <input type="password" onChange={(event) =>{
-      setLoginPassword(event.target.value)
-    }}  />Password
-    <button onClick={login}>Login</button>
-  </div>
-  <div>
-    <h1>User Logged In</h1> {user?.email}
-  </div>
-
-  <div>
-    <button onClick={logout}>Log Out</button>
-  </div>
-
-
-      
     </>
   )
 }
